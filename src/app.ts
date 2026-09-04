2a67ab521345b72fee468984e66fa6306b7f67a7
Afiqu3 <gahossainj@gmail.com>
2026-09-04 17:57:38 +0600

    Implement middleware for error handling, request validation, and not found responses; add JWT utility functions and refactor app structure

--- src/app.ts
+++ src/app.ts
@@ -1,20 +1,22 @@
 import cookieParser from "cookie-parser";
 import cors from "cors";
 import express, {
-    type Application,
-    type Request,
-    type Response,
+	type Application,
+	type Request,
+	type Response,
 } from "express";
 import httpStatus from "http-status";
 import config from "./app/config";
+import { globalErrorHandler } from "./app/middleware/globalErrorHandler";
+import { notFound } from "./app/middleware/notFound";
 
 const app: Application = express();
 
 app.use(
-    cors({
-        origin: config.frontend_url,
-        credentials: true,
-    }),
+	cors({
+		origin: config.frontend_url,
+		credentials: true,
+	}),
 );
 
 // Enable URL-encoded form data parsing
@@ -45,10 +47,13 @@ app.use(cookieParser());
 
 // Basic route
 app.get("/", async (req: Request, res: Response) => {
-    res.status(httpStatus.OK).json({
-        success: true,
-        message: "Welcome to ParcelFlow System Backend",
-    });
+	res.status(httpStatus.OK).json({
+		success: true,
+		message: "Welcome to ParcelFlow System Backend",
+	});
 });
+
+app.use(globalErrorHandler);
+app.use(notFound);
 
 export default app;
