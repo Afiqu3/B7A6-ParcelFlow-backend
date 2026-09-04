21c9daed99e33dc0ad54030c4faa10c2157b1ec2
Afiqu3 <gahossainj@gmail.com>
2026-09-04 10:01:49 +0600

    Initialize project structure with Prisma configuration, Express app setup, and environment variables

---
+++ src/app/config/index.ts
@@ -0,0 +1,12 @@
+import dotenv from "dotenv";
+import path from "path";
+
+dotenv.config({ path: path.join(process.cwd(), ".env") });
+
+export default {
+	node_env: process.env.NODE_ENV,
+	port: process.env.PORT,
+	database_url: process.env.DATABASE_URL,
+	bak_url: process.env.APP_URL,
+	frontend_url: process.env.FRONTEND_URL,
+};
